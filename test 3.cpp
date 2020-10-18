2a7b457b38279715840c13e640939f6e6b9d1aac
angelinveena <73064324+angelinveena@users.noreply.github.com>
2020-10-18 19:31:10 +0530

    Add files via upload

---
+++ test 3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"20";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"\nYou are eligible";
+	}
+	else
+	    cout<<"\nYou are not eligible";
+	    return 0;
+}
